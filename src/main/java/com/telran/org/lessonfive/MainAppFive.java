31be0efec801badb93f54919993608b7362def90
IuriiAv <Yurasha22r22@>
2024-07-03 00:21:30 +0300

    add lessons five

---
+++ src/main/java/com/telran/org/lessonfive/MainAppFive.java
@@ -0,0 +1,20 @@
+package com.telran.org.lessonfive;
+
+public class MainAppFive {
+
+    public static void main(String[] args) {
+        int num = 5;
+
+        //if - one string or one {}
+        //else - one string or one {}
+
+        if (num > 10) {
+            System.out.println("Hello");
+            System.out.println("JavaScript");
+        } else {
+            System.out.println("Java");
+        }
+
+        System.out.println("End program");
+    }
+}
---
+++ src/main/java/com/telran/org/lessonfive/MainAppFour.java
@@ -0,0 +1,13 @@
+package com.telran.org.lessonfive;
+
+public class MainAppFour {
+    //empty string
+    public static void main(String[] args) {
+        int num = 15;
+        if (num == 15) {
+            System.out.println("num=15");
+        } else {
+            System.out.println("I am not in if");
+        }
+    }
+}
