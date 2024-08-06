1c24ab3687bef6f89a87a925a03fb187083fe6e7
IuriiAv <avramenko.ca@gmail.com>
2024-08-06 23:00:01 +0300

    add bubblesort explanation

--- src/main/java/com/telran/org/lessonnine/BubbleSort.java
+++ src/main/java/com/telran/org/lessonnine/BubbleSort.java
@@ -6,10 +6,13 @@ public class BubbleSort {
 
     public static void main(String[] args) {
         int[] ints = {11, 5, 6, 7, 3, 5, 7, 1, 3, 2, 8, 12, 3};
-
+        // 5 6 7 3 5 7 1 3 2 8 11 3 12
+        // 5 6 3 5 7 1 3 2 7 8 3 11 12
         System.out.println("Before " + Arrays.toString(ints));
 
+        // 0 - 12
         for (int i = 0; i < ints.length; i++) {
+            //0 - 11
             for (int j = 0; j < ints.length - 1 - i; j++) {
                 if (ints[j] > ints[j + 1]) {
                     swap(ints, j);
